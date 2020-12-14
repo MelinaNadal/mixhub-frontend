7efd7f86177a65056193e3ab33d4f848c08a3a76
Melina Nadal <melinanhopkins@gmail.com>
2020-12-14 16:43:45 -0500

    updates API key

--- src/services/cocktailService.js
+++ src/services/cocktailService.js
@@ -1,7 +1,7 @@
-fetch("https://the-cocktail-db.p.rapidapi.com/filter.php? i=${searchTerm}", {
+fetch(`https://the-cocktail-db.p.rapidapi.com/filter.php? i=${searchTerm}`, {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-key": "SIGN-UP-FOR-KEY",
+		"x-rapidapi-key": "c0f227240amsh55aa92563c9e45cp1e4b12jsn976987091817",
 		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com"
 	}
 })
